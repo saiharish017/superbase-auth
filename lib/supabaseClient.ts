9ccd3a13341ef8fcef473dd604b91e566d5c9f2d
saiharish017 <saiharish017@gmail.com>
2024-08-23 18:29:31 +0530

    superbase

---
+++ lib/supabaseClient.ts
@@ -0,0 +1,7 @@
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
+
+// Initialize Supabase client with the environment variables
+const supabaseUrl: string = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
+const supabaseAnonKey: string = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
+
+export const supabase: SupabaseClient = createClient(supabaseUrl, supabaseAnonKey);
